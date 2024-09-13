77507f3e55626861891e5d985088a2165836f8e9
cmtlyt <cmt01964896@taobao.com>
2024-09-13 16:57:31 +0800

    chore: 添加antd依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ConfigProvider } from 'antd';
 import { RouterView } from './router';
 
 function App() {
-  return <RouterView />;
+  return (
+    <ConfigProvider theme={{}}>
+      <RouterView />
+    </ConfigProvider>
+  );
 }
 
 export default App;
