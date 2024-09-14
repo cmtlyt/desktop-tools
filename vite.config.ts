cb15cb61c909eabf1138ca785b9c70e964056117
cmtlyt <cmt01964896@taobao.com>
2024-09-14 09:42:03 +0800

    chore: 添加代码验证工具

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import viteStylelint from 'vite-plugin-stylelint';
+// @ts-expect-error not type
+import viteEslint from 'vite-plugin-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -9,7 +12,14 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStylelint({
+      // 对某些文件排除检查
+      exclude: 'node_modules',
+    }),
+    viteEslint(),
+  ],
   // 设置路径别名
   resolve: {
     alias: {
