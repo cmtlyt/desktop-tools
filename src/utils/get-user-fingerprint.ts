574a6693d5cdff6eb79ee0d01206d50a26a05097
cmtlyt <cmt01964896@taobao.com>
2024-10-18 17:10:34 +0800

    feat: 用户指纹变更埋点

--- src/utils/get-user-fingerprint.ts
+++ src/utils/get-user-fingerprint.ts
@@ -1,15 +1,20 @@
 import { USER_FINGERPRINT_KEY } from '@/constant';
 import { load } from '@fingerprintjs/fingerprintjs';
+import { logger } from '.';
+
+let userFingerprint: string;
 
 load()
   .then((fp) => fp.get())
   .then((result) => result.visitorId)
   .then((visitorId) => {
-    localStorage.setItem(USER_FINGERPRINT_KEY, visitorId);
-    return visitorId;
+    userFingerprint ||= localStorage.getItem(USER_FINGERPRINT_KEY) || '';
+    if (userFingerprint && userFingerprint !== visitorId) {
+      logger.warn('user fingerprint changed', { old: userFingerprint, curr: visitorId });
+      localStorage.setItem(USER_FINGERPRINT_KEY, visitorId);
+      userFingerprint = visitorId;
+    }
   });
-
-let userFingerprint: string;
 
 export function getUserFingerprint() {
   return (userFingerprint ||= localStorage.getItem(USER_FINGERPRINT_KEY) || '');
