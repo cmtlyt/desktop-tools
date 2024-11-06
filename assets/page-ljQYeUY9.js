2cfd0f3249072c322c0e45e29ec78edae2bd28b8
cmtlyt <cmtlyt@users.noreply.github.com>
2024-11-06 02:40:08 +0000

    deploy: 7b083a1a28aed5cb3f4c7f74e052c9ae69614b8e

---
+++ assets/page-ljQYeUY9.js
@@ -0,0 +1,18 @@
+import{y as e}from"./index-DhqkE82_.js";import{d as t,b as o,c as r,A as a,l as s}from"./index-qbPds83G.js";import{G as p,A as c}from"./index-DlPsbaM7.js";import{S as m}from"./icon-DbJv4tdA.js";import"./index-DDjNEWPb.js";import"./index-Cp1vlVfz.js";import"./index-DPPQToFW.js";const n=t(o)`
+  flex: 1;
+
+  &::before,
+  &::after {
+    content: '';
+    flex: 1;
+    box-shadow:
+      inset 0 0 0 0.4rem currentColor,
+      inset 0 0 0 0.8rem #fff,
+      inset 0 0 0 10rem currentColor;
+  }
+`,l=t(o)`
+  height: 80%;
+  aspect-ratio: 1/1;
+`;function x(){return e.jsxs(l,{$gap:"0.5",children:[e.jsx(n,{$gap:"0.5",$direction:r.COLUMN}),e.jsx(n,{$gap:"0.5",$direction:r.COLUMN})]})}const d=p,f=[{name:"十步万度",path:"./sbwd",icon:e.jsx(m,{})},{name:"俄罗斯方块",path:"./elsfk",icon:e.jsx(x,{})},{name:"扫雷",path:"./sl",icon:e.jsx(d,{})}],h=t(o)`
+  padding: var(--page-padding);
+`;function C(){return e.jsx(a,{onFirstAppear:()=>s.appear("game-list"),children:e.jsx(h,{children:e.jsx(c,{appListHander:()=>f.map(i=>({...i,contentStyle:{width:"10rem",height:"10rem"},iconStyle:{fontSize:"8rem"},labelStyle:{fontSize:"1.8rem"}}))})})})}const $={title:"游戏列表",crumbLabel:"列表"};export{C as Component,$ as handle};
--- assets/use-navigate-DkqdL1gq.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./index-BjqInUx_.js";import{a3 as o,r as u}from"./index-CjNiMQur.js";function g(){const t=o();return u.useCallback((a,e)=>(r().setLoading(!0),typeof a=="number"?t(a):t(a,e)),[t])}export{g as u};
