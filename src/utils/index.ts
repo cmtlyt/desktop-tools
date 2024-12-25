00107bf64da45355206b165eb76ba756d6bcce47
cmtlyt <cmt01964896@taobao.com>
2024-12-25 15:17:56 +0800

    fix: 使用monitor代替logger

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { getDeviceInfo } from '@cmtlyt/base';
-import { createLogger, Kind } from '@cmtlyt/logger';
+import { createMonitor, Kind } from '@cmtlyt/monitor';
 import { getPageInfo } from '@/components/sync-page-info';
 import { isProd, LOGGER_STORAGE_KEY } from '@/constant';
 import { filterForJson } from './filter';
@@ -22,20 +22,32 @@ interface LoggerExtendOptions {
   };
 }
 
-const logger = createLogger<ExtendKind, LoggerExtendOptions>({
-  needTrace: !isProd,
-  logConfig: {
-    click: { kind: 'click', inherit: 'info' },
-    event: { kind: 'event', inherit: 'info' },
-    appear: { kind: 'appear', inherit: 'info', needTrace: false },
-    todo: { kind: 'todo', inherit: 'warn', needTrace: true },
-    error: { kind: 'error', needTrace: true },
-    expose: { kind: 'expose', inherit: 'info', needTrace: false },
+const logger = createMonitor<ExtendKind, LoggerExtendOptions>({
+  loggerOptions: {
+    needTrace: !isProd,
+    logConfig: {
+      system: { kind: 'system', inherit: 'info' },
+      click: { kind: 'click', inherit: 'info' },
+      event: { kind: 'event', inherit: 'info' },
+      appear: { kind: 'appear', inherit: 'info', needTrace: false },
+      todo: { kind: 'todo', inherit: 'warn', needTrace: true },
+      error: { kind: 'error', needTrace: true },
+      expose: { kind: 'expose', inherit: 'info', needTrace: false },
+    },
+    // 生产环境下不在控制台输出
+    printFunc: isProd ? () => {} : null,
+    getPrintFunc(kind) {
+      const { printToDebug } = this.store;
+      if (printToDebug.includes(kind)) {
+        return (...args) => console.debug(...args);
+      }
+      return (...args) => console.log(...args);
+    },
   },
   store: {
     exposeCache: [],
-    needExposeKind: ['click', 'appear', 'error', 'event', 'expose'],
-    printToDebug: ['click', 'appear', 'debug', 'event'],
+    needExposeKind: ['click', 'appear', 'error', 'event', 'expose', 'system'],
+    printToDebug: ['click', 'appear', 'debug', 'event', 'system'],
     getLoggerBaseInfo() {
       return { pageInfo: getPageInfo(), userFingerprint: getUserFingerprint(), deviceInfo: getDeviceInfo() };
     },
@@ -58,29 +70,29 @@ const logger = createLogger<ExtendKind, LoggerExtendOptions>({
       if (exposeCache.length >= 10 || info.kind === 'expose') this.expose();
     },
   },
-  // 生产环境下不在控制台输出
-  printFunc: isProd ? () => {} : null,
-  getPrintFunc(kind) {
-    const { printToDebug } = this.store;
-    if (printToDebug.includes(kind)) {
-      return (...args) => console.debug(...args);
-    }
-    return (...args) => console.log(...args);
+  formatLogInfo(info) {
+    return info.logInfo;
   },
-  onLogBefore(e) {
+  reportStrategy(info) {
     const { needExposeKind } = this.store;
-    const { kind } = e;
-    if (needExposeKind.includes(kind)) {
-      try {
-        this.store.messagesHandler(e.messages);
-      } catch (err) {
-        logger.error('logger.store.messagesHandler', { err: (err as Error).stack, messages: e.messages, kind });
-      }
-      this.store.exposeHandler({ kind, info: e.messages, time: Date.now() });
-      if (isProd) {
-        e.preventDefault();
-        // TODO: 线上日志上报
-      }
+    const { kind } = info;
+    return needExposeKind.includes(kind);
+  },
+  formatReportInfo(info) {
+    try {
+      this.store.messagesHandler(info.messages);
+    } catch (err) {
+      const { kind, messages } = info;
+      logger.error('logger.store.messagesHandler', { err: (err as Error).stack, messages, kind });
+    }
+    return info;
+  },
+  reportLog(info) {
+    const { kind } = info;
+    this.store.exposeHandler({ kind, info: info.messages, time: Date.now() });
+    if (isProd) {
+      info.preventDefault();
+      // TODO: 线上日志上报
     }
   },
 });
