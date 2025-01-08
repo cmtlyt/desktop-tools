0ce8430001be07e096b1ee54f2c27b91ef74908d
cmtlyt <cmt01964896@taobao.com>
2025-01-08 09:58:05 +0800

    chore: bump config

--- bump.config.ts
+++ bump.config.ts
@@ -3,7 +3,7 @@ import { execSync } from 'node:child_process';
 
 export default defineConfig({
   // commit 消息模板, %s 会替换为版本号, 传 false 表示不生成 commit
-  commit: 'release: publish version v%s',
+  commit: 'chore: release version v%s',
   // commit 消息打 git 标签, %s 会替换为版本号, 传 false 表示不打 tag
   tag: 'v%s',
   // 是否推送 commit 和 tag, 传入 false 表示不推送
@@ -13,13 +13,12 @@ export default defineConfig({
   // 在发布之前是否需要检查未处理的 git 树, 传入 true 表示不检查
   noGitCheck: false,
   // 是否跳过 git 提交时的钩子, 传入 true 表示跳过
-  noVerify: false,
+  noVerify: true,
   // 配置要修改版本号的文件
   files: ['package.json'],
   execute: (config) => {
     execSync('git add .');
-    console.log(config);
-    execSync(`git commit -m "chore: release v${config.state.tagName}" --no-verify`);
+    execSync(`git commit -m "chore: release v${config.state.newVersion}" --no-verify`);
     execSync('git push');
   },
   // 是否需要确认修改, 传入 true 表示需要确认
