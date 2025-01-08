a813ef4be6945dd24fca5a3c3b584d480bf0ac4e
cmtlyt <cmt01964896@taobao.com>
2025-01-08 09:47:25 +0800

    chore: 作者没有mac开发者账户故移除mac包支持

--- bump.config.ts
+++ bump.config.ts
@@ -10,12 +10,12 @@ export default defineConfig({
   // 是否提交所有文件, 传入 false 表示只提交 bumpp 更改的文件
   all: true,
   // 在发布之前是否需要检查未处理的 git 树, 传入 true 表示不检查
-  noGitCheck: true,
+  noGitCheck: false,
   // 是否跳过 git 提交时的钩子, 传入 true 表示跳过
   noVerify: false,
   // 配置要修改版本号的文件
   files: ['package.json'],
-  execute: 'git add .',
+  execute: 'git add . && git commit -m "chore: release v%s" && git push',
   // 是否需要确认修改, 传入 true 表示需要确认
   confirm: false,
 });
